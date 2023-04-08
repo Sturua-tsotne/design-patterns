e5be2694ca03aa2739ec9f0fc816b4263756a6db
ცოტნე სტურუა <47318145+Sturua-tsotne@users.noreply.github.com>
2023-04-08 23:19:17 +0400

    add singlton pattern

---
+++ SingltonDemo/TableServers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class TableServers
+    {
+
+        private static readonly TableServers _instance = new();
+        private readonly List<string> servers = new();
+        private int nextServer = 0;
+
+        private TableServers()
+        {
+            servers.Add("tim");
+            servers.Add("sue");
+            servers.Add("mary");
+            servers.Add("bob");
+
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+
+        public string GetNextServer()
+        {
+            string output = servers[nextServer];
+            nextServer++;
+            if (nextServer >= servers.Count)
+                nextServer = 0;
+
+            return output;
+        }
+
+    }
+}
